2f9da4dff574285571eaf1a6eff24c7e866f4ebe
yahia <y_kilany@aucegypt.edu>
2023-11-30 23:00:56 +0200

    created an RNG and a the load rom and the fontset

--- Chip8.h
+++ Chip8.h
@@ -1,9 +1,13 @@
+#ifndef CHIP8_H
+#define CHIP8_H
 #include <cstdint>
+#include <chrono>
+#include <random>
 class Chip8
 {
     public:
         uint8_t registers[16]{};
-	    uint8_t memory[4096]{};
+        uint8_t memory[4096]{};
         uint16_t index{};
         uint16_t pc{};
         uint16_t stack[16]{};
@@ -12,5 +16,9 @@ class Chip8
         uint8_t soundTimer{};
         uint8_t Keys{};
         uint32_t video[64 * 32]{};
+        std::default_random_engine randGen;
+        std::uniform_int_distribution<uint8_t> randByte;
+        Chip8();
         void LoadROM(char const* filename);
 };
+#endif // CHIP8_H
--- LoadRom.cpp
+++
@@ -1,4 +0,0 @@
-#include <fstream>
-#include <Chip8.h>
-
-void Chip8::LoadROM(char const* filename){}
